37af63b5fc06b8551011b8bae8159e14f9bf1bcb
Brandon Benefield <bsquared18@gmail.com>
2020-03-02 10:45:01 -0500

    Add ability to change CMS branch depending on env variable 'dev' These env variables are controlled by which app start command you use
    - webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,23 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const common = require("./webpack.common");
 
+/**/
+const fs = require('fs')
+const cmsConfigFilePath = path.join(process.cwd(), './site/static/admin/config.yml')  // e.g. ~/dev/one-click-hugo-cms/site/static/admin/config.yml
+// this line READS the contents of a file and converts it to human readable utf-8
+fs.readFile(cmsConfigFilePath, 'utf-8', (err, data) => {
+  if (err) {
+    throw new Error(err)  // throw error if any
+  }
+  // This line takes the file contents and replaces the branch name with an env variable we set in our npm script e.g. "npm run dev"
+  fs.writeFile(cmsConfigFilePath, data.replace(/branch: \w+/, `branch: ${process.env.BRANCH}`), (err) => {
+    if (err) {
+      throw new Error(err)  // throw error if any
+    }
+  })
+})
+/**/
+
 module.exports = merge(common, {
   mode: "development",
 
